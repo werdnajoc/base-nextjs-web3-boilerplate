7515f29d9faed0275965d9266a1b6b632fe6a964
Andrew Ortiz <werdnajoc@gmail.com>
2023-03-03 23:21:35 -0300

    fix: metatags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,29 @@
 import Script from 'next/script'
-import type {AppProps} from 'next/app'
-import {useRouter} from 'next/router'
-import {PersistGate} from "redux-persist/integration/react";
-import {useStore} from "react-redux";
-
+import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { PersistGate } from 'redux-persist/integration/react'
+import { useStore } from 'react-redux'
 
 import 'shared/assets/styles/app.sass'
 
 import HeadGlobal from 'components/HeadGlobal'
-import {reduxWrapper} from 'providers/redux'
-import {AppProviders} from 'providers'
+import { reduxWrapper } from 'providers/redux'
+import { AppProviders } from 'providers'
 
-function App({Component, pageProps}: AppProps) {
-    const store: any = useStore();
-    const router = useRouter()
+function App({ Component, pageProps }: AppProps) {
+  const store: any = useStore()
+  const router = useRouter()
 
-    return (
-        <PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
-            <AppProviders>
-                <HeadGlobal/>
-                <Component key={router.asPath} {...pageProps} />
+  return (
+    <>
+      <HeadGlobal />
 
-                <Script id="show-banner">
-                    {`
+      <PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
+        <AppProviders>
+          <Component key={router.asPath} {...pageProps} />
+
+          <Script id="show-banner">
+            {`
             (function () {
               // Change these if you use something different in your hook.
               var storageKey = 'darkMode';
@@ -61,11 +62,11 @@ function App({Component, pageProps}: AppProps) {
               }
             })();
           `}
-                </Script>
-            </AppProviders>
-        </PersistGate>
-
-    )
+          </Script>
+        </AppProviders>
+      </PersistGate>
+    </>
+  )
 }
 
 export default reduxWrapper.withRedux(App)
